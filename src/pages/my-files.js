78f359099d04a1284d131f653a083efed9e68de6
saksham-kapoor <sakshamkapoor1729@gmail.com>
2020-05-14 23:33:59 +0530

    Section 4,5,6,7

---
+++ src/pages/my-files.js
@@ -0,0 +1,50 @@
+import React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
+
+const MyFiles = ({ data }) => {
+  return (
+    <Layout>
+      <div>
+        <h1>My Site's Files</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>relativePath</th>
+              <th>prettySize</th>
+              <th>extension</th>
+              <th>birthTime</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allFile.edges.map(({ node }, index) => (
+              <tr key={index}>
+                <td>{node.relativePath}</td>
+                <td>{node.prettySize}</td>
+                <td>{node.extension}</td>
+                <td>{node.birthTime}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allFile {
+      edges {
+        node {
+          birthTime(fromNow: true)
+          relativePath
+          prettySize
+          extension
+        }
+      }
+    }
+  }
+`
+
+export default MyFiles
---
+++ src/pages/pandas-love-cats.md
@@ -0,0 +1,10 @@
+---
+title: "Pandas Love Cats"
+date: "2019-04-15"
+---
+
+Cats are really sweet.
+
+Pandas are just lovely as well. Together they are irresistable :)
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4n0xNbfJLR8" frameborder="0" allowfullscreen></iframe>
